e094ef3f4a69e9fb5ac86551ea9fc85e48f93882
QDZantoine <antoine.quendez@gmail.com>
2024-05-17 15:41:39 +0200

    add bootstrap and fontawesome #1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+// Import your routes here
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const NotFound = () => (
+  <h1>Not Found</h1>
+);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        {/* Add your routes here */}
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
