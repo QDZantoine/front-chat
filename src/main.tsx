c71256b2783b8c6fa0febd06de96ddae7c130b7f
marsender <didier.corbiere@opale-concept.com>
2024-05-20 12:20:07 +0200

    move the navbar in the application component #8

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes,Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-// Import your routes here
-import conversationRoutes from './routes/conversation'
-import messageRoutes from './routes/message';
-import userRoutes from './routes/user';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -14,14 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App/>}/>
-        { conversationRoutes }
-        { messageRoutes }
-        { userRoutes }
-        <Route path='*' element={<Navigate replace to="/" />} />
-      </Routes>
-    </Router>
+		<App />
   </React.StrictMode>
 );
